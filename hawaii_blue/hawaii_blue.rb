49e570d6f1007f10da129d666d1cbdb3d53eb9ca
Jed Schneider <jed.schneider@gmail.com>
2011-06-22 18:33:32 -0400

    refactoring basic property specs

--- hawaii_blue/hawaii_blue.rb
+++ hawaii_blue/hawaii_blue.rb
@@ -4,19 +4,21 @@ require 'seasonal_property'
 require 'rubygems'
 require 'json'
 
-begin
-  reservation_file = File.open(ARGV.detect{|arg| arg[/.txt$/, 0] })
-  rentals  = JSON.parse(File.read(ARGV.detect{|arg| arg[/.json$/, 0] }))
-rescue
-  raise IOError, 
-  <<-DOC 
-     you need to give me some files
-     like:
-     `ruby hawaii_blue.rb vaction_rentals.json reservations.txt`
-     extensions are sensitive, the names are not
-    DOC
-end
+#begin
+#  reservation_file = File.open(ARGV.detect{|arg| arg[/.txt$/, 0] })
+#  rentals  = JSON.parse(File.read(ARGV.detect{|arg| arg[/.json$/, 0] }))
+#rescue
+#  raise IOError, 
+#  <<-DOC 
+#     you need to give me some files
+#     like:
+#     `ruby hawaii_blue.rb vaction_rentals.json reservations.txt`
+#     extensions are sensitive, the names are not
+#    DOC
+#end
 
+reservation_file = File.read("fixtures/input.txt")
+rentals = JSON.parse(File.read("fixtures/vacation_rentals.json"))
 out = []
 
 reservation_file.each do |reservation|
--- hawaii_blue/spec/basic_property_spec.rb
+++ hawaii_blue/spec/basic_property_spec.rb
@@ -1,10 +1,16 @@
 require 'spec_helper'
 
-describe BasicProperty do
-  it "should define the nightly rate" do
-    property = {"name"=>"Paradise Inn", "rate"=>"$250", "cleaning fee"=>"$120"}
-    property.extend BasicProperty
-    property.nightly_rate.should == 250.0
-    property.nightly_rate(Date.new).should == 250.0
+describe "BasicProperty" do
+  let(:property) {(Object.new().extend BasicProperty) }
+
+  describe "nightly rate" do
+    subject {property}
+    it { should respond_to(:nightly_rate).with(1).argument }
+    it { should respond_to(:nightly_rate).with(0).arguments }
+
+    it "should define the nightly rate" do
+      property = {"name"=>"Paradise Inn", "rate"=>"$250", "cleaning fee"=>"$120"}.extend BasicProperty
+      property.nightly_rate(Date.new).should == 250.0
+    end
   end
 end
