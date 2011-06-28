c1939981edae486ad4ca6b95de5008547627eb9c
Jed Schneider <jed.schneider@gmail.com>
2011-06-28 12:27:14 -0400

    adding discussion points for code review

--- hawaii_blue/lib/property.rb
+++ hawaii_blue/lib/property.rb
@@ -8,13 +8,6 @@ module Property
     "%s: $%.2f" % [name, total(dates)]
   end
 
-  def total(dates)
-    matches = dates.scan(/(\d{4}\/\d{2}\/\d{2})/)
-    range = Date.parse(matches[0][0])...Date.parse(matches[1][0])
-    nights = range.map(&:to_s)
-    (rental_cost(nights) + cleaning_fee) * sales_tax
-  end
-
   def name
     self['name']
   end
@@ -23,11 +16,22 @@ module Property
     1.0411416
   end
 
+  # Discussion Point: #rental_cost and #total are defined here
+  # but it is dependent on the nightly rate which needs to be defined by the 
+  # SeasonalProperty BasicProperty modules, this seems like 
+  # violation of OOP but its DRY. I feel like this is the weakest part of my implementation
   def rental_cost(nights)
     nights.map{|night| nightly_rate(night)}.reduce(:+)
   end
+  
+  def total(dates)
+    matches = dates.scan(/(\d{4}\/\d{2}\/\d{2})/)
+    range = Date.parse(matches[0][0])...Date.parse(matches[1][0])
+    nights = range.map(&:to_s)
+    (rental_cost(nights) + cleaning_fee) * sales_tax
+  end
 
-  def nightly_rate(date="")
+  def nightly_rate(date=nil)
     raise NotImplementedError
   end
 end
--- hawaii_blue/spec/spec_helper.rb
+++ hawaii_blue/spec/spec_helper.rb
@@ -4,7 +4,8 @@ require 'lib/basic_property'
 
 shared_examples_for "a property" do
   subject { Hash.new().extend described_class}
-  it {should respond_to(:name, :sales_tax, :cleaning_fee, :quote, :rental_cost, :nightly_rate) }
+  it {should respond_to(:name, :sales_tax, :cleaning_fee, :quote, :rental_cost, 
+                        :nightly_rate, :total, :rental_cost) }
   it { should respond_to(:nightly_rate).with(1).argument }
   it { should respond_to(:nightly_rate).with(0).arguments }
 end
